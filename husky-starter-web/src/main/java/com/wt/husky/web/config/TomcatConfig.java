1524f318e08f990c59077a2f9dcd0bc250a5d5ef
qiyu <jsj0804wt@126.com>
2022-03-10 19:39:08 +0800

    引入undertow by qiyu 20220-03-10

--- husky-starter-web/src/main/java/com/wt/husky/web/config/TomcatConfig.java
+++ husky-starter-web/src/main/java/com/wt/husky/web/config/TomcatConfig.java
@@ -1,17 +1,16 @@
 package com.wt.husky.web.config;
 
-import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
 import org.apache.catalina.startup.Tomcat;
+import org.apache.coyote.http2.Http2Protocol;
 import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 /**
  * @author qiyu
@@ -20,34 +19,30 @@ import org.springframework.context.annotation.Primary;
 @Configuration
 @ConditionalOnProperty(value = "server.ssl.enabled", havingValue = "true")
 @ConditionalOnClass(Tomcat.class)
-public class TomcatConfig {
+public class TomcatConfig implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
 
-    @Bean
-    public Connector connector(@Value("${server.port:443}") int port, @Value("${http.port:80}") int httpPort) {
+    @Value("${server.port:443}")
+    private int port;
+
+    @Value("${http.port:80}")
+    private int httpPort;
+
+    @Override
+    public void customize(TomcatServletWebServerFactory factory) {
         Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
         connector.setScheme("http");
         connector.setPort(httpPort);
         connector.setSecure(false);
         connector.setRedirectPort(port);
-        return connector;
+        connector.addUpgradeProtocol(new Http2Protocol());
+        factory.addAdditionalTomcatConnectors(connector);
+        factory.addContextCustomizers(context -> {
+            SecurityConstraint securityConstraint = new SecurityConstraint();
+            securityConstraint.setUserConstraint("CONFIDENTIAL");
+            SecurityCollection collection = new SecurityCollection();
+            collection.addPattern("/*");
+            securityConstraint.addCollection(collection);
+            context.addConstraint(securityConstraint);
+        });
     }
-
-    @Bean
-    @Primary
-    public TomcatServletWebServerFactory tomcatServletWebServerFactory(Connector connector) {
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-            @Override
-            protected void postProcessContext(Context context) {
-                SecurityConstraint securityConstraint = new SecurityConstraint();
-                securityConstraint.setUserConstraint("CONFIDENTIAL");
-                SecurityCollection collection = new SecurityCollection();
-                collection.addPattern("/*");
-                securityConstraint.addCollection(collection);
-                context.addConstraint(securityConstraint);
-            }
-        };
-        tomcat.addAdditionalTomcatConnectors(connector);
-        return tomcat;
-    }
-
 }
--- husky-starter-web/src/main/java/com/wt/husky/web/config/UndertowConfig.java
+++ husky-starter-web/src/main/java/com/wt/husky/web/config/UndertowConfig.java
@@ -1,36 +1,30 @@
 package com.wt.husky.web.config;
 
-import io.undertow.Undertow;
 import io.undertow.UndertowOptions;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.EnvironmentAware;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
 
 /**
  * @author qiyu
  * @date 2022/1/30
  */
-@Configuration
-@ConditionalOnClass(Undertow.class)
-@ConditionalOnExpression
-public class UndertowConfig {
+public class UndertowConfig implements WebServerFactoryCustomizer<UndertowServletWebServerFactory>, EnvironmentAware {
 
-    @Bean
-    public UndertowServletWebServerFactory undertowServletWebServerFactory(ConfigurableEnvironment environment) {
-        UndertowServletWebServerFactory factory = new UndertowServletWebServerFactory();
+    private Environment environment;
+
+    @Override
+    public void customize(UndertowServletWebServerFactory factory) {
         if (Boolean.valueOf(environment.getProperty("server.http2.enabled", "false")))
             factory.addBuilderCustomizers(builder -> builder.setServerOption(UndertowOptions.ENABLE_HTTP2, true));
         if (Boolean.valueOf(environment.getProperty("server.ssl.enabled", "false")))
             factory.addBuilderCustomizers(builder -> builder.addHttpListener(
                     Integer.valueOf(environment.getProperty("http.port", "80")), "0.0.0.0"));
-        return factory;
     }
 
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
+    }
 }
