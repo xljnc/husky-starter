e1ac8212af1cacb8d40f3f87641a52c7cbd6bc13
qiyu <jsj0804wt@126.com>
2022-03-20 23:02:27 +0800

    引入junit by qiyu 2022-03-20

---
+++ husky-starter-test-demo/src/test/java/com/wt/husky/ApplicationTest.java
@@ -0,0 +1,11 @@
+package com.wt.husky;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author qiyu
+ * @date 2022/3/20
+ */
+@SpringBootApplication
+public class ApplicationTest {
+}
---
+++ husky-starter-test-demo/src/test/java/com/wt/husky/ScanTest.java
@@ -0,0 +1,24 @@
+package com.wt.husky;
+
+import com.wt.husky.redis.util.RedisUtil;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author qiyu
+ * @date 2022/3/20
+ */
+@SpringBootTest
+public class ScanTest {
+
+    @Autowired
+    private RedisUtil redisUtil;
+
+    @Test
+    public void createData() {
+        for (int i = 0; i < 10000; i++) {
+            redisUtil.setStringValue("key" + i, String.valueOf(i));
+        }
+    }
+}
