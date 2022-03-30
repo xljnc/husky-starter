408fc3c5fbd011c9df5ebbfd421f35494b4d87d4
qiyu <jsj0804wt@126.com>
2022-03-30 21:11:56 +0800

    key HyperLogLog操作 by qiyu 2022-03-30

--- husky-starter-test-demo/src/test/java/com/wt/husky/test/RedisTest.java
+++ husky-starter-test-demo/src/test/java/com/wt/husky/test/RedisTest.java
@@ -1,14 +1,16 @@
 package com.wt.husky.test;
 
 import com.wt.husky.redis.util.RedisUtil;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -23,20 +25,23 @@ public class RedisTest {
     @Autowired
     private RedisUtil redisUtil;
 
-    @Test
-    @BeforeAll
-    public void createData() {
-        Map<String, String> people1 = new HashMap<>();
-        for (int i = 0; i < 10000; i++) {
-            redisUtil.setString("key" + i, String.valueOf(i), 1800L, TimeUnit.SECONDS);
-            people1.put("key" + i, "value" + i);
-            redisUtil.setHash("people2", "key" + i, "value" + i);
-        }
-        redisUtil.setObject("people1", people1);
-    }
+//    @Test
+//    @BeforeAll
+//    public void createData() {
+//        Map<String, String> people1 = new HashMap<>();
+//        for (int i = 0; i < 10000; i++) {
+//            redisUtil.setString("key" + i, String.valueOf(i), 1800L, TimeUnit.SECONDS);
+//            people1.put("key" + i, "value" + i);
+//            redisUtil.setHash("people2", "key" + i, "value" + i);
+//        }
+//        redisUtil.setObject("people1", people1);
+//    }
 
     @Test
     public void scan() {
+        for (int i = 0; i < 10000; i++) {
+            redisUtil.setString("key" + i, String.valueOf(i), 1800L, TimeUnit.SECONDS);
+        }
         Set<String> keys = redisUtil.scan("key*", 1000);
         System.out.println(keys);
         System.out.println(keys.size());
@@ -44,6 +49,9 @@ public class RedisTest {
 
     @Test
     public void testGetMultiHashValues() {
+        for (int i = 0; i < 10000; i++) {
+            redisUtil.setHash("people2", "key" + i, "value" + i);
+        }
         List<Object> hashKeys = Arrays.asList("key1", "key2", "key3");
         List<Object> values = redisUtil.getMultiHashValues("people2", hashKeys);
         System.out.println(hashKeys);
@@ -52,9 +60,23 @@ public class RedisTest {
 
     @Test
     public void testScanHash() {
+        for (int i = 0; i < 10000; i++) {
+            redisUtil.setHash("people2", "key" + i, "value" + i);
+        }
         Set<Map.Entry<Object, Object>> entries = redisUtil.scanHash("people2", "key*", 1000);
         System.out.println(entries.size());
         System.out.println(entries);
     }
 
+    @Test
+    public void testHyperLogLog() {
+        for (int i = 0; i < 10000; i++) {
+            redisUtil.addHyperLogLogValue("pfKey1", "key" + i);
+            redisUtil.addHyperLogLogValue("pfKey2", "key" + i);
+        }
+        redisUtil.mergeHyperLogLog("pfKey3", "pfKey1","pfKey2");
+        System.out.println(redisUtil.sizeOfHyperLogLog("pfKey1"));
+        System.out.println(redisUtil.sizeOfHyperLogLog("pfKey2"));
+        System.out.println(redisUtil.sizeOfHyperLogLog("pfKey3"));
+    }
 }
