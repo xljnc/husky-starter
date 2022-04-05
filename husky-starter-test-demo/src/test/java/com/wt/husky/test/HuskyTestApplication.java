53cd37cce84be3dd50b0f3d422b2e9ba7a15ad67
qiyu <jsj0804wt@126.com>
2022-04-05 22:54:11 +0800

    引入Caffine by qiyu 2022-04-05

--- husky-starter-test-demo/src/test/java/com/wt/husky/test/HuskyTestApplication.java
+++ husky-starter-test-demo/src/test/java/com/wt/husky/test/HuskyTestApplication.java
@@ -1,11 +1,13 @@
 package com.wt.husky.test;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 /**
  * @author qiyu
  * @date 2022/3/20
  */
 @SpringBootApplication
+@EnableCaching
 public class HuskyTestApplication {
 }
