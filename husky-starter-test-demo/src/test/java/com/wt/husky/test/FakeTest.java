97fe6a51badf35b77556b13172b323506cbb8199
qiyu <jsj0804wt@126.com>
2022-04-18 10:32:02 +0800

    fake test by qiyu 2022-04-18

--- husky-starter-test-demo/src/test/java/com/wt/husky/test/FakeTest.java
+++ husky-starter-test-demo/src/test/java/com/wt/husky/test/FakeTest.java
@@ -1,10 +1,17 @@
 package com.wt.husky.test;
 
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
 /**
  * description
  *
  * @author wutian2@myhexin.com
  * @date 2022/4/18
  */
+@SpringBootTest
+@ActiveProfiles("local")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class FakeTest {
 }
