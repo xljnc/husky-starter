2e5bf846ef5c1b325d85f7d215c936d711918bd9
qiyu <jsj0804wt@126.com>
2022-05-14 17:25:05 +0800

    mongodb文档 by qiyu 2022-05-04

---
+++ doc/mongodb.sql
@@ -0,0 +1,21 @@
+//交叉索引
+db.product.insertMany{
+[
+{"name":"a","price":60,"stock":10},
+{"name":"b","price":100,"stock":2},
+{"name":"c","price":20,"stock":8},
+{"name":"d","price":40,"stock":7},
+{"name":"e","price":80,"stock":1}
+]
+}
+db.product.createIndex({
+   price:1},{
+   background:true})
+db.product.createIndex({
+   stock:1},{
+   background:true})
+
+//可以使用交叉索引
+db.product.find({"price":20,"stock":8}).explain();
+//不能使用交叉索引
+db.product.find({"price":20}).sort({"stock":1}).explain();
