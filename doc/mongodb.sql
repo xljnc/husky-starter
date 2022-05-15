cbe13b58f50246b32413474c5195eee6c73d83e3
qiyu <jsj0804wt@126.com>
2022-05-15 20:10:41 +0800

    mongodb文档 by qiyu 2022-05-15

--- doc/mongodb.sql
+++ doc/mongodb.sql
@@ -22,7 +22,8 @@ db.product.find({"price":20}).sort({"stock":1}).explain();
 
 //覆盖索引
 db.product.drop();
-db.product.insertMany(
+db.product.insertMany
+(
 [
 {"name":"a","price":60,"stock":10},
 {"name":"b","price":100,"stock":2},
@@ -30,12 +31,37 @@ db.product.insertMany(
 {"name":"d","price":40,"stock":7},
 {"name":"e","price":80,"stock":1}
 ]
-)
-db.product.createIndex({
+);
+db.product.createIndex
+({
    price:1,stock:1},{
-   background:true})
+   background:true});
 //走覆盖索引
-db.product.find({"price":20,"stock":{$lte:10}},{_id:0,name:0}).explain("executionStats");
+db.product.find({price:20},{stock:1,_id:0}).explain("executionStats");
 //不走覆盖索引
-db.product.find({"price":20,"stock":{$lte:10}}).explain("executionStats");
+db.product.find({price:20},{stock:1}).explain("executionStats");
 
+//过期索引
+db.product.drop();
+db.product.createIndex
+(
+   {createDate: 1},{expireAfterSeconds: 10},{background:true}
+);
+db.product.insertMany
+(
+[
+{"name":"a","createDate":new Date("2022-05-11")},
+{"name":"b","createDate":new Date("2022-05-11")},
+{"name":"c","createDate":new Date("2022-05-11")},
+{"name":"d","createDate":new Date("2022-05-11")},
+{"name":"e","createDate":new Date("2022-05-11")}
+]
+);
+
+db.product.createIndex
+({"expireDate": 1},{expireAfterSeconds: 0})
+db.product.insertOne(
+{"name":"f","expireDate":new Date("2022-05-14T00:00:00.000Z")}
+);
+db.product.find
+();
